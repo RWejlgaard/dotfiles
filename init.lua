f113214ba698c621509eced0ee08eeb5f4801ac3
Rasmus Wejlgaard <pez@pez.sh>
2024-07-20 23:13:17 +0100

    vim improvements

--- init.lua
+++ init.lua
@@ -2,57 +2,37 @@
 local fn = vim.fn
 local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
 if fn.empty(fn.glob(install_path)) > 0 then
-    packer_bootstrap = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim',
-                                  install_path})
+    Packer_bootstrap = fn.system({ 'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim',
+        install_path })
 end
-
-vim.api.nvim_set_keymap('n', '<leader>\\', '<Plug>(easymotion-s)', {
-    noremap = true,
-    silent = true
-})
 
 -- plugins
 local use = require('packer').use
 require('packer').startup(function()
-    use {'Xuyuanp/nerdtree-git-plugin'}
-    use {'airblade/vim-gitgutter'}
-    -- use { 'airblade/vim-rooter' }
-    use {'antoinemadec/FixCursorHold.nvim'}
-    use {'easymotion/vim-easymotion'}
-    use {'hashivim/vim-terraform'}
+    use { 'airblade/vim-gitgutter' }            -- show git changes in the gutter
+    use { 'easymotion/vim-easymotion' }         -- easy motion with "\\"
+    use { 'hashivim/vim-terraform' }            -- terraform syntax highlighting
     use {
         'junegunn/fzf',
         run = 'fzf#install()'
     }
     use {'junegunn/fzf.vim'}
-    use { 'EdenEast/nightfox.nvim' }
-    use {'morhetz/gruvbox'}
-    use {'nvim-treesitter/nvim-treesitter'}
-    use {'scrooloose/nerdcommenter'}
-    -- use { 'scrooloose/nerdtree' }
-    use {'tpope/vim-fugitive'}
-    use {'tpope/vim-surround'}
-    -- use { 'vim-airline/vim-airline' }
-    use {'itchyny/lightline.vim'}
-    use {'josa42/nvim-lightline-lsp'}
-    use {'wookayin/fzf-ripgrep.vim'}
-    use {'yuki-yano/fzf-preview.vim'}
-    use {'wbthomason/packer.nvim'}
-    use {'godlygeek/tabular'}
-    use {'fatih/vim-go'}
-    use {'folke/tokyonight.nvim'}
-    use {'Yazeed1s/oh-lucy.nvim'}
-    use {"ellisonleao/glow.nvim"}
-    use {"nvim-lua/lsp-status.nvim"}
-    use {'nvim-lua/plenary.nvim'}
-    use {'MunifTanjim/nui.nvim'}
-    use {'dpayne/CodeGPT.nvim'}
-    use {'towolf/vim-helm'}
-    use {'onsails/lspkind.nvim'}
-    use {'zbirenbaum/copilot.lua'}
+    use { 'EdenEast/nightfox.nvim' }            -- nightfox theme
+    use { 'nvim-treesitter/nvim-treesitter' }   -- treesitter, makes syntax highlighting better
+    use { 'scrooloose/nerdcommenter' }          -- easy commenting
+    use { 'tpope/vim-fugitive' }                -- git integration with :G{git cmd}
+    use { 'itchyny/lightline.vim' }             -- statusline
+    use { 'wookayin/fzf-ripgrep.vim' }
+    use { 'yuki-yano/fzf-preview.vim' }
+    use { 'wbthomason/packer.nvim' }
+    use { 'fatih/vim-go' }                      -- go syntax highlighting
+    use { "ellisonleao/glow.nvim" }
+    use { 'dpayne/CodeGPT.nvim' }
+    use { 'onsails/lspkind.nvim' }
+    use { 'zbirenbaum/copilot.lua' }
     use {
         "zbirenbaum/copilot-cmp",
-        after = {"copilot.lua"},
+        after = { "copilot.lua" },
         config = function()
             require("copilot_cmp").setup()
         end
@@ -62,29 +42,20 @@ require('packer').startup(function()
     use { 'CopilotC-Nvim/CopilotChat.nvim' }
     use {
         "iamcco/markdown-preview.nvim",
-        requires = {"zhaozg/vim-diagram", "aklt/plantuml-syntax"}
-    }
-    use {
-        "folke/zen-mode.nvim",
-        config = function()
-            require("zen-mode").setup {
-                -- your configuration comes here
-                -- or leave it empty to use the default settings
-                -- refer to the configuration section below
-            }
-        end
+        requires = { "zhaozg/vim-diagram", "aklt/plantuml-syntax" }
     }
     use {
         'romgrk/barbar.nvim',
-        requires = {'kyazdani42/nvim-web-devicons'}
+        requires = { 'kyazdani42/nvim-web-devicons' }
     }
-    use({
-        "catppuccin/nvim",
-        as = "catppuccin"
-    })
     use {
-        'kyazdani42/nvim-tree.lua',
-        requires = {'kyazdani42/nvim-web-devicons'}
+        "nvim-neo-tree/neo-tree.nvim",
+        branch = "v3.x",
+        requires = {
+            "nvim-lua/plenary.nvim",
+            "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+            "MunifTanjim/nui.nvim",
+        }
     }
     use {
         "folke/trouble.nvim",
@@ -100,32 +71,31 @@ require('packer').startup(function()
             require('nvim-terminal').setup()
         end
     }
-    use {"williamboman/mason.nvim", "williamboman/mason-lspconfig.nvim", "neovim/nvim-lspconfig"}
+    use { "williamboman/mason.nvim", "williamboman/mason-lspconfig.nvim", "neovim/nvim-lspconfig" }
     use {
         'VonHeikemen/lsp-zero.nvim',
-        requires = { -- LSP Support
-        {'neovim/nvim-lspconfig'}, {'williamboman/nvim-lsp-installer'}, -- Autocompletion
-        {'hrsh7th/nvim-cmp'}, {'hrsh7th/cmp-buffer'}, {'hrsh7th/cmp-path'}, {'saadparwaiz1/cmp_luasnip'},
-        {'hrsh7th/cmp-nvim-lsp'}, {'hrsh7th/cmp-nvim-lua'}, -- Snippets
-        {'L3MON4D3/LuaSnip'}, {'rafamadriz/friendly-snippets'}}
+        requires = {                                                    -- LSP Support
+            { 'neovim/nvim-lspconfig' }, { 'williamboman/nvim-lsp-installer' }, -- Autocompletion
+            { 'hrsh7th/nvim-cmp' }, { 'hrsh7th/cmp-buffer' }, { 'hrsh7th/cmp-path' }, { 'saadparwaiz1/cmp_luasnip' },
+            { 'hrsh7th/cmp-nvim-lsp' }, { 'hrsh7th/cmp-nvim-lua' },     -- Snippets
+            { 'L3MON4D3/LuaSnip' }, { 'rafamadriz/friendly-snippets' } }
     }
 end)
 
 -- settings
 vim.opt.termguicolors = true
-vim.api.nvim_set_option('guifont', 'FiraCode Nerd Font:h12')
--- vim.api.nvim_set_option('NERDTreeChDirMode', 2)
-vim.api.nvim_set_option('number', true)
-vim.api.nvim_set_option('smarttab', true)
-vim.api.nvim_set_option('tabstop', 8)
-vim.api.nvim_set_option('softtabstop', 0)
-vim.api.nvim_set_option('expandtab', true)
-vim.api.nvim_set_option('shiftwidth', 4)
-vim.api.nvim_set_option('backspace', '2')
-vim.api.nvim_set_option('laststatus', 2)
-vim.api.nvim_set_option('mouse', 'a')
-vim.api.nvim_set_option('clipboard', 'unnamed')
-vim.api.nvim_set_option('scrolloff', 17)
+vim.opt.guifont = 'FiraCode Nerd Font:h12'
+vim.opt.number = true
+vim.opt.smarttab = true
+vim.opt.tabstop = 8
+vim.opt.softtabstop = 0
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 4
+vim.opt.backspace = '2'
+vim.opt.laststatus = 2
+vim.opt.mouse = 'a'
+vim.opt.clipboard = 'unnamed'
+vim.opt.scrolloff = 17
 vim.cmd('set tabstop=8 softtabstop=0 expandtab shiftwidth=4 smarttab')
 vim.opt.number = true
 vim.opt.colorcolumn = '80'
@@ -136,11 +106,11 @@ local opts = {
     noremap = true,
     silent = true
 }
-vim.api.nvim_set_keymap('n', 'nt', ':NvimTreeToggle<CR>', opts)
+vim.api.nvim_set_keymap('n', 'nt', ':Neotree toggle<CR>', opts)
 vim.api.nvim_set_keymap('n', 'qqq', ':qall<CR>', opts)
 vim.api.nvim_set_keymap('n', '<C-f>', ':Files<CR>', opts)
 vim.api.nvim_set_keymap('n', '<leader>/', ':Rg<CR>', opts)
-vim.api.nvim_set_keymap('n', '<leader>\\', '<Plug>(easymotion-s)', opts)
+vim.api.nvim_set_keymap('n', '<leader>\\', '<Plug>(easymotion-s1)', opts)
 
 vim.api.nvim_set_keymap('n', '<leader>v', ':vsplit<CR>', opts)
 vim.api.nvim_set_keymap('n', '<leader>h', ':split<CR>', opts)
@@ -164,33 +134,40 @@ require("copilot").setup({
     }
 })
 
-require("nvim-tree").setup({
-    sort = {
-        sorter = "case_sensitive"
-    },
-    view = {
-        width = 50
-    },
-    renderer = {
-        group_empty = true
-    },
-    filters = {
-        dotfiles = true
-    },
-    actions = {
-        open_file = {
-            quit_on_open = true,
-        },
-    },
-})
-
 require("nvim-lsp-installer").setup {}
+
+require("neo-tree").setup {
+    close_on_open = false,
+    close_if_last_window = true,
+    window = {
+        width = 40,
+        side = "left",
+        auto_resize = true,
+        mappings = {
+            ["o"] = "open"
+        }
+    },
+    filesystem = {
+        hijack_netrw_behavior = "open_current"
+    }
+}
+
+-- Open NeoTree on startup when no file is specified
+--vim.api.nvim_create_augroup('NeoTreeOnStartup', { clear = true })
+--vim.api.nvim_create_autocmd('VimEnter', {
+  --group = 'NeoTreeOnStartup',
+  --callback = function()
+    --if vim.fn.argc() == 0 then
+      --vim.cmd('Neotree toggle')
+    --end
+  --end
+--})
 
 -- Language Server
 local lsp_zero = require('lsp-zero')
 local cmp = require('cmp')
 
-lsp_zero.on_attach(function(client, bufnr)
+lsp_zero.on_attach(function(_, bufnr)
     lsp_zero.default_keymaps({
         buffer = bufnr
     })
@@ -230,15 +207,15 @@ cmp.setup({
     formatting = {
         format = lspkind.cmp_format({
             mode = 'symbol', -- show only symbol annotations
-            maxwidth = 50, -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
-            -- can also be a function to dynamically calculate max width such as 
+            maxwidth = 50,   -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
+            -- can also be a function to dynamically calculate max width such as
             -- maxwidth = function() return math.floor(0.45 * vim.o.columns) end,
-            ellipsis_char = '...', -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
+            ellipsis_char = '...',    -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
             show_labelDetails = true, -- show labelDetails in menu. Disabled by default
 
             -- The function below will be called before any actual modifications from lspkind
             -- so that you can provide more controls on popup customization. (See [#30](https://github.com/onsails/lspkind-nvim/pull/30))
-            before = function(entry, vim_item)
+            before = function(_, vim_item)
                 -- do some customizations ...
                 return vim_item
             end
@@ -267,7 +244,7 @@ cmp.setup({
             select = true
         }) -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
     }),
-    sources = cmp.config.sources({{
+    sources = cmp.config.sources({ {
         name = 'copilot'
     }, {
         name = 'nvim_lsp'
@@ -276,12 +253,12 @@ cmp.setup({
     }, {
         name = 'path'
     } -- For vsnip users.
-    -- { name = 'luasnip' }, -- For luasnip users.
-    -- { name = 'ultisnips' }, -- For ultisnips users.
-    -- { name = 'snippy' }, -- For snippy users.
-    }, {{
+        -- { name = 'luasnip' }, -- For luasnip users.
+        -- { name = 'ultisnips' }, -- For ultisnips users.
+        -- { name = 'snippy' }, -- For snippy users.
+    }, { {
         name = 'buffer'
-    }})
+    } })
 })
 
 -- enable diagnostics for showing in-line
