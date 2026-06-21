2320c3fe58fa71ef2ddccb785663f7c514b90dd9
Rasmus "Pez" Wejlgaard <pez@pez.sh>
2026-06-21 09:57:18 +0100

    fix: automatically start tmux (#26)

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,2 +1,8 @@
 set -g fish_greeting "" # Silence welcome message
 set -gx EDITOR nvim
+
+if status is-interactive
+    and not set -q TMUX
+    and command -q tmux
+    exec tmux new -As stuff
+end
